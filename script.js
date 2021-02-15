c1fcd116412ee8356e45d1e0f67c06d7ddc9e475
ridenby <ridenby@gmail.com>
2021-02-15 22:21:43 +0400

    добавлены новые стили и разметка

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 'use strict';
 
-let arr = ['23ffw', 'ergwer', '3434fg', 'asasas'],
-    i = arr.join(', ');
+let tenzor = document.querySelectorAll('.circle'),
+    box = document.querySelectorAll('.box'),
+    wrapper = document.querySelector('.wrapper');
 
-console.log(i);
+// tenzor.forEach((item, index, arr) => {
+//     item.style.backgroundColor = 'pink';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('Тут был и я');
+
+div.textContent = 'Privet <p></p>Mir';
+div.classList.add('black');
+
+document.body.insertBefore(div, tenzor[0]);
+
+console.log(div);
